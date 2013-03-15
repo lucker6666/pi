0cfc2117a7a64c5b96e26516cf37abee9d7dddb5
Dave Shah <david.a.shah@gmail.com>
2013-03-15 12:07:00 -0300

    Rename

--- build.sh
+++ build.sh
@@ -3,4 +3,4 @@ ant clean
 ant build
 ant install
 adb forward tcp:9090 tcp:54767
-adb shell uiautomator runtest alchemyhour.jar -c com.leandog.driver.Driver
+adb shell uiautomator runtest pi.jar -c com.leandog.driver.Driver
